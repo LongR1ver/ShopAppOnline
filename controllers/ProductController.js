81e8f13cef38b821918e40e1c6c5685e70310a11
Truong Giang Nguyen <giangnt470@gmail.com>
2025-03-17 23:06:59 +1100

    Some updates in product controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -3,15 +3,6 @@ import db from "../models"
 import InsertProductRequest from "../dtos/requests/InsertProductRequest"
 
 export async function addProduct(req, res) {
-    const {error} = InsertProductRequest.validate(req.body)
-
-    if (error) {
-        return res.status(400).json({
-            message: "Error occurs when adding product",
-            error: error.details[0]?.message
-        })
-    }
-
     const product = await db.Product.create(req.body)
 
     return res.status(201).json({
